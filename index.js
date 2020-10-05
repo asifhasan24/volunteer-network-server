eadb8449eac0c6052dcaa3094cd195eeb156ad36
asifhasan24 <asifmehu24@gmail.com>
2020-10-05 17:29:59 +0600

    done

--- index.js
+++ index.js
@@ -1,52 +1,73 @@
 const express = require('express')
-require('dotenv').config
-
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const port = 7000
-const app = express()
-
-
-app.use(cors())
-app.use(bodyParser.json())
-const pass = "asif2189"
-
+const bodyParser=require('body-parser');
+const cors =require('cors');
+const { ObjectID } = require('mongodb');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://asifhasan:asif2189@cluster0.gbfwa.mongodb.net/volunteer-network?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(cors());
+
 client.connect(err => {
-    const collection = client.db("volunteer-network").collection("activities");
-    console.log('connected successfully')
-
-    app.post('/addInfo', (req, res) => {
-        const newInfo = req.body
-        collection.inserOne(newInfo) 
-            .then(result => {
-                res.send(result.insertedCount > 0)
-            })
-        console.log(newInfo)
-    })
-
-     app.get('/info',(req,res)=>{
-         collection.find({email: req.query.email})
-         .toArray((err,documents)=>{
-             res.send(documents)
-         })
-     })
-
-  
-
+  const works = client.db("volunteer-network").collection("works");
+  const registers = client.db("volunteer-network").collection("registers");
+app.post('/addWorks',(req,res)=>{
+  const work=req.body;
+  works.insertOne(work)
+  .then(result=>{
+    res.send(result.insertedCount>0)
+  })
+})
+app.get('/works',(req,res)=>{
+ works.find({})
+ .toArray((err,document)=>{
+   res.send(document)
+ })  
+})
+app.get('/works/:id',(req,res)=>{
+  const id=req.params.id;
+  works.find({_id:ObjectID(id)})
+  .toArray((err,document)=>{
+    res.send(document[0])
+  })  
+ })
+ app.post('/registration',(req,res)=>{
+  const register=req.body;
+  registers.insertOne(register)
+  .then(result=>{
+    console.log(result)
+    res.send(result.insertedCount>0)
+  })
+})
+app.get('/registers',(req,res)=>{
+  registers.find({email:req.query.email})
+  .toArray((err,document)=>{
+    res.send(document)
+  })  
+ })
+ app.get('/registersList',(req,res)=>{
+  registers.find({})
+  .toArray((err,document)=>{
+    res.send(document)
+  })  
+ })
+ app.delete('/delete/:id',(req,res)=>{
+   const key=req.params.id
+  registers.deleteOne({_id:key})
+  .then(result=>{
+       res.send(result.deletedCount>0)
+  })
+})
+app.delete('/deleteEvent/:id',(req,res)=>{
+  const key=(req.params.id)
+ registers.deleteOne({_id:key})
+ .then(result=>{
+      res.send(result.deletedCount>0)
+ })
+})
 });
 
-
-app.get('/', (req, res) => {
-    res.send('Hellooo World!')
-})
-
-app.listen(port, () => {
-    console.log('running')
-})
+app.listen(5000)
